42e6433007eed549986ca5c173ee27e27133b992
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-04-30 11:31:51 +0200

    Create TaskMapApplication.java

---
+++ src/main/java/com/example/taskmap/TaskMapApplication.java
@@ -0,0 +1,13 @@
+package com.example.taskmap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TaskMapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TaskMapApplication.class, args);
+	}
+
+}
