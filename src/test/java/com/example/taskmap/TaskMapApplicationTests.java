c0e6a8c053311c0a659808a30db279a65f5f56fd
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-04-30 11:31:57 +0200

    Create TaskMapApplicationTests.java

---
+++ src/test/java/com/example/taskmap/TaskMapApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.taskmap;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TaskMapApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
