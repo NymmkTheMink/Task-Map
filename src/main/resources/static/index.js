f553193fdb5c419888731b6fb9254dff59dd2a84
NymmkTheMink <141179443+NymmkTheMink@users.noreply.github.com>
2024-05-27 11:02:20 +0200

    Update index.js

--- src/main/resources/static/index.js
+++ src/main/resources/static/index.js
@@ -14,3 +14,31 @@ function helpPanel(){
 function downloadManuel(){
 
 }
+
+function createProjectPanel() {
+    fetch('create-project-panel.html')
+        .then(response => response.text())
+        .then(data => document.querySelector('#project-creator-panel').innerHTML = data);
+}
+
+function submitProject(event) {
+    event.preventDefault();
+
+    const projectName = document.getElementById('projectName').value;
+
+    fetch('/api/projects', {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json',
+        },
+        body: JSON.stringify({ name: projectName }),
+    })
+        .then(response => response.json())
+        .then(data => {
+            console.log('Success:', data);
+            alert('Form successfully submitted!'); // Add this line
+        })
+        .catch((error) => {
+            console.error('Error:', error);
+        });
+}
